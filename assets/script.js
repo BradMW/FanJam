af3be6a89ef9b519765d5d33fd32bc3529240ad5
Phillip <phillip.welch2@mavs.uta.edu>
2021-10-04 15:33:28 -0500

    merging

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 var ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&keyword=machinegunkelly&apikey=2AXpKaz2osoCIVl9Uly7i4JgRllUmxfL`;
 
-var artist = '';
-var title = '';
+var artist = 'linkin park';
+var title = 'breaking habits';
 var lyricsUrl = `https://api.lyrics.ovh/v1/${artist}/${title}`;
 
-fetch()
+fetch(lyricsUrl)
   .then(function (response) {
     return response.json();
   })
