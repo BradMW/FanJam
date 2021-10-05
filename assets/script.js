62c78258f78729173227331472745ab6c870360f
Phillip <phillip.welch2@mavs.uta.edu>
2021-10-05 11:03:02 -0500

    commiting before a pull

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-var ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${artist}&apikey=2AXpKaz2osoCIVl9Uly7i4JgRllUmxfL`;
 var artist = '';
 var title = '';
 var searchHistory = [];
@@ -32,19 +31,18 @@ search.on("click", function(event) {
 });
 
 
-function ticketMasterApi(ticketMasterUrl) {
-  
-  ticketMasterUrl = `https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=${artist}&apikey=2AXpKaz2osoCIVl9Uly7i4JgRllUmxfL`;
-    fetch(lyricsUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-          console.log(data);
-        //   append lyrics to maybe p tags in the first column
-          getHistory()
-      });
-  }
+function lyricsApi() {
+  var lyricsUrl = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+  fetch(lyricsUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+      //   append lyrics to maybe p tags in the first column
+        getHistory()
+    });
+}
 
 function getHistory(){
     var searchHistoryDiv = //$("#historyBtns");
@@ -77,7 +75,7 @@ function getHistory(){
 
 
 function attractions() {
-    var attractionsURL = `https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=${artist}&apikey=2AXpKaz2osoCIVl9Uly7i4JgRllUmxfL`
+    var attractionsURL = `https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=${artist}&apikey=2AXpKaz2osoCIVl9Uly7i4JgRllUmxfL`;
     fetch(attractionsURL)
         .then(function (response) {
             return response.json();
