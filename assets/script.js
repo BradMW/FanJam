170630687b353665bafd1991471b0e909b4a3241
Sue Lee <sue.lee0308@gmail.com>
2021-10-05 11:16:55 -0500

    trying calls

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 var artist = '';
 var title = '';
 var searchHistory = [];
-var search = //grab html search button
+var search = $(".search");
 
 
 search.on("click", function(event) {
     event.preventDefault();
     // take input value to create city's name variable to pass to 5 day forecast function
-    var artist = $(".inputArea1").val();
-    var title = $(".inputArea2").val();
+    artist = $(".inputArea1").val();
+    title = $(".inputArea2").val();
     console.log(artist);
 
     // need to add city name to buttons
@@ -33,6 +33,7 @@ search.on("click", function(event) {
 
 function lyricsApi() {
     var lyricsUrl = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    console.log(lyricsUrl);
     fetch(lyricsUrl)
       .then(function (response) {
         return response.json();
@@ -45,7 +46,7 @@ function lyricsApi() {
   }
 
 function getHistory(){
-    var searchHistoryDiv = //$("#historyBtns");
+    var searchHistoryDiv = $("#historyBtns");
     searchHistoryDiv.html("");
 
     if(localStorage.getItem("title")) {
@@ -56,7 +57,7 @@ function getHistory(){
         
         // for loop to create buttons of history of city searches
         for (var i = 0; i < searchHistory.length; i++) {
-            var newBtns = //$("<button class='btn btn-primary' type='button'>Search</button>")
+            var newBtns = $("<button class='btn btn-primary' type='button'>Search</button>")
             console.log(newBtns);
             newBtns.text(searchHistory[i]);
             searchHistoryDiv.append(newBtns);
@@ -84,17 +85,18 @@ function attractions() {
             console.log(data);
 
         // capitalize all letters from input
-        //   if (data.attractions.name.toUpperCase() === artist.toUpperCase && data.attractions.upcomingEvents._total > 0) {
+        // if (data.attractions[].name.toUpperCase() === artist.toUpperCase && data.attractions.upcomingEvents._total > 0) {
             // make all the variables for the buttons(if statement for if the artist has a link to their fb, youtube, etc)
             // prepend photo to lyrics side of columns
             // append social media buttons
             // call events function
-            // events(artist)};
+            events(artist)
             // else{
                 // append the "this artist has no upcoming events" to html}
         
-        })
-  }
+        // })
+  })
+}
 
   function events(artist) {
       var eventsURL = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${artist}&apikey=2AXpKaz2osoCIVl9Uly7i4JgRllUmxfL`
@@ -108,9 +110,9 @@ function attractions() {
         if (data.events.name.toUpperCase() === artist.toUpperCase) {
             for (var i=0; i < data.events.length; i++) {
                 var concertURL = data.events[i].url;
-                var concertLink = $("<a class='button'> </a>");
+                var concertLink = $("<button><a class='button'></a></button>");
                 concertLink.attr("href", concertURL);
-                // cardDiv.append(concertLink);
+                cardDiv.append(concertLink);
             }       
     //     //else {
     //         // append the "this artist has no upcoming events" to html}
@@ -120,4 +122,4 @@ function attractions() {
 
 // function for "search new artist" button at the bottom of both columns that scrolls user back to the top of the page
 // clear html inner.HTML("")
-// append buttons to the bottom of the lyrics with title of the song. Create an array in local storage to do this.
+// append buttons to the bottom of the lyrics with title of the song. Create an array in local storage to do this
